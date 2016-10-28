ab31f315fb8c29abdfe33141b96f6ba706028a09
Tam Nguyen <mr.tamron@gmail.com>
2016-10-28 14:58:53 -0700

    add delete question button

--- app/routes/question.js
+++ app/routes/question.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('question', params.question_id);
+  },
+  actions: {
+    delete(question) {
+      if (confirm('Are you sure you want to delete question?')) {
+        question.destroyRecord();
+        this.transitionTo('index');
+      }
+    }
   }
 });
