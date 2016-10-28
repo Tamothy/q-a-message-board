718cb8e5e19782a962c3f8ab5ed5802f808543e9
Tam Nguyen <mr.tamron@gmail.com>
2016-10-28 10:28:11 -0700

    add one-to-many relationship with question/answers

--- app/models/answer.js
+++ app/models/answer.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   content: DS.attr(),
-  author: DS.attr()
+  author: DS.attr(),
+  answers: DS.belongsTo('question', {async:true})
 });
--- app/models/question.js
+++ app/models/question.js
@@ -2,5 +2,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   content: DS.attr(),
-  author: DS.attr()
+  author: DS.attr(),
+  notes: DS.attr(),
+  answers: DS.hasMany('answer', {async:true})
 });
