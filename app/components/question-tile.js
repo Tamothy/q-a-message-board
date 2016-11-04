643813245f145aad0526faf4b0add67aecabf4af
Tam Nguyen <mr.tamron@gmail.com>
2016-11-04 08:43:42 -0700

    add question voting count

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    upVote(question) {
+      this.sendAction('upVote', question);
+    },
+
+    downVote(question) {
+      this.sendAction('downVote', question);
+    }
+  }
 });
